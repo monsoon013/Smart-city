9a7a20b153c089c88c8e58c56fd4217cfc8d47f5
monsoon013 <cael.corredera.perez@gmail.com>
2026-03-20 17:29:11 +0100

    Menú creado (sin switch) + clases, interfaces creadas

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,23 @@ public class Main {
         int op = 0;
 
         while(op < 6){
+            System.out.println("###BIENVENIDX A SMART CITY###");
+            System.out.println("1.Añadir un nuevo dispositivo" +
+                               "\n2.Mostrar estado de la red" +
+                               "\n3.Lanzar mantenimiento global" +
+                               "\n4.Usando el operador instanceof" +
+                               "\n5.Reinicio remoto de emergencia" +
+                               "\n6.Salir");
+            System.out.println("Elige una opción: ");
+            op = scan.nextInt();
+            scan.nextLine(); //limpieza diaria del buffer
+            switch(op){
+                case 1: {
+                    System.out.println("Indica el tipo de dispositivo a añadir: ");
+                    String newDisp = scan.nextLine();
+
+                }
+            }
 
         }
     }

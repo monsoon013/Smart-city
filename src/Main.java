058ed50e48d2c4842f1b42b6fcb03be082d37787
monsoon013 <cael.corredera.perez@gmail.com>
2026-03-20 17:21:10 +0100

    Inicialización del main

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+public class Main {
+    public static void main (String [] args){
+        Scanner scan = new Scanner(System.in);
+        int op = 0;
+
+        while(op < 6){
+
+        }
+    }
+}
